1149ba28be13b2d25488563cba6234f50fbbad8f
cyb-don <ajosepholuwaseun@gmail.com>
2026-05-01 08:19:30 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -60,8 +60,8 @@ getUser()
 
 function userData(user) {
     // const greetName = user.firstName.slice(0, 1).toUpperCase() + user.firstName.slice(1);
-    document.getElementById("welcome").innerText = `Hello, €{user.firstName}!`;
-    document.getElementById("availBalance").innerText = `₦${user.acctBalance}`;
+    document.getElementById("welcome").innerText = `Hello, {user.firstName}!`;
+    document.getElementById("availBalance").innerText = `€${user.acctBalance}`;
     spreadTransactions(user.transactions)
 
 };
